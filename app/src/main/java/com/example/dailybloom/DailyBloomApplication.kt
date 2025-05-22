873390ff7695a262a212094f543dd91f4fd81592
AsunaIU <valnevann@gmail.com>
2025-05-22 14:34:39 +0500

    homework_9: fix an App class for Hilt

--- app/src/main/java/com/example/dailybloom/DailyBloomApplication.kt
+++ app/src/main/java/com/example/dailybloom/DailyBloomApplication.kt
@@ -4,8 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DailyBloomApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
+class DailyBloomApplication : Application()
